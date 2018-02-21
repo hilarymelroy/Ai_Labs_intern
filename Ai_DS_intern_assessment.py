baa588becbe81f709aa2e10262d4fa56c558e05c
hilarymelroy <hilarymelroy@gmail.com>
2018-02-20 21:26:25 -0500

    Added code to iterate through summaries, save the words and produce a word cloud minus stop words.

--- Ai_DS_intern_assessment.py
+++ Ai_DS_intern_assessment.py
@@ -1,11 +1,8 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Feb 19 19:44:39 2018
+Created on Tue Feb 20 18:05:58 2018
 
 @author: hrm1886
-
-Ai labs data science intern assessment
-
 """
 import numpy as np
 import pandas as pd
@@ -14,15 +11,9 @@ import os
 from collections import Counter
 import re
 from nltk.corpus import stopwords
-
-os.chdir('C:\\Users\\hrm1886\\Desktop')
-
-'''
-This function saves movie data in a pandas dataframe and creates
- a dictionary with genres as keys and frequency of occurances of words
- as values. It outputs the top 5 genres in the movie data.
-It assumes a file type of csv.
-'''
+from wordcloud import WordCloud
+from wordcloud import WordCloud, STOPWORDS
+import matplotlib.pyplot as plt
 
 movie_data = pd.read_csv('movie_data.csv')
 
@@ -45,5 +36,53 @@ def top_genres(df):
 top_5_genres = print(top_genres(movie_data))
  
             
+def genre_word_summary(df,genre):
+    new_movie_data = movie_data[movie_data['genres'].str.contains(genre)]
+    summary_list = []
+    for summary in new_movie_data['summary']:
+        summary_list.append(summary)
+    return summary_list
+words_summary = genre_word_summary(movie_data,'drama')
 
-            
+all_words_list = []
+for word_string in words_summary:
+    word_string = re.sub(r'\W+', ' ',word_string)
+    word_string = word_string.split(" ")
+    for i in word_string:
+        all_words_list.append(i)
+        
+def remove_stopwords(word_list):
+        processed_word_list = []
+        for word in word_list:
+            word = word.lower() # in case they arenet all lower cased
+            if word not in stopwords.words("english"):
+                processed_word_list.append(word)
+        return processed_word_list
+new_words_summary = remove_stopwords(all_words_list)
+
+new_words_text = ' '.join(new_words_summary)
+
+stopwords = set(STOPWORDS)
+print(stopwords)
+
+def show_wordcloud(data, title = None):
+    wordcloud = WordCloud(
+        background_color='white',
+        stopwords=stopwords,
+        max_words=100,
+        max_font_size=40, 
+        scale=3,
+        random_state=1 # chosen at random by flipping a coin; it was heads
+    ).generate(str(data))
+
+    fig = plt.figure(1, figsize=(12, 12))
+    plt.axis('off')
+    if title: 
+        fig.suptitle(title, fontsize=20)
+        fig.subplots_adjust(top=2.3)
+
+    plt.imshow(wordcloud)
+    plt.savefig('Drama_Wordcloud.jpg')
+    plt.show()
+    
+show_wordcloud(new_words_text)
